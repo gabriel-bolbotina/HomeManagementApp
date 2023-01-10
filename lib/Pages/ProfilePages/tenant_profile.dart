2369e0460d2e3a66ae66b63054500b711ca8ef18
Milana Utica <milanaaautica@gmail.com>
2023-01-11 00:26:09 +0200

    created link with firebase for profile editing fields

--- lib/Pages/ProfilePages/tenant_profile.dart
+++ lib/Pages/ProfilePages/tenant_profile.dart
@@ -60,7 +60,7 @@ class _TenantProfilePageWidgetState
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50),
         child: AppBar(
-          backgroundColor: FlutterFlowTheme.of(context).primaryBtnText,
+          backgroundColor: FlutterFlowTheme.of(context).lineColor,
           automaticallyImplyLeading: false,
           leading:IconButton(
             icon: const Icon(Icons.arrow_back,
@@ -75,6 +75,21 @@ class _TenantProfilePageWidgetState
       body: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+            child: Container(
+              width: 100,
+              height: 100,
+              clipBehavior: Clip.antiAlias,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              alignment: Alignment.topRight,
+              child: Image.network(
+                'https://picsum.photos/seed/339/600',
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
             child: Row(
